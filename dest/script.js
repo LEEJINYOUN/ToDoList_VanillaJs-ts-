f1dbfc010aeac8349fd89955fd0b4a9490ba4a99
이진영 <swlsdud1005@naver.com>
2023-06-22 10:54:28 +0900

    할 일 추가 기능

--- dest/script.js
+++ dest/script.js
@@ -1,1 +1,83 @@
 "use strict";
+const taskInput = document.querySelector(".taskInput");
+const addBtn = document.querySelector(".addBtn");
+const taskBoard = document.querySelector(".taskBoard");
+let taskList = [];
+const tabs = document.querySelectorAll(".taskTabs div");
+let mode = "all";
+let filterList = [];
+const all = document.querySelector("#all");
+const onGoing = document.querySelector("#ongoing");
+const done = document.querySelector("#done");
+addBtn === null || addBtn === void 0 ? void 0 : addBtn.addEventListener("click", addTask);
+taskInput === null || taskInput === void 0 ? void 0 : taskInput.addEventListener("keyup", (e) => {
+    if (e.keyCode === 13) {
+        addTask();
+    }
+});
+function randomId() {
+    return "_" + Math.random().toString(36).substring(2, 9);
+}
+function render() {
+    let list = [];
+    if (mode == "all") {
+        list = taskList;
+    }
+    else if (mode == "ongoing" || mode == "done") {
+        list = filterList;
+    }
+    let resultHTML = "";
+    for (let i = 0; i < list.length; i++) {
+        if (list[i].isComplete == true) {
+            resultHTML += `
+        <div class="task">
+        <div class="taskTitle">
+          <div class="title taskDone" >${list[i].taskContent}</div>
+        </div>
+        <div class="taskBtns">
+          <button class="checkBtn" onClick="toggleComplete('${list[i].id}')">
+          <i class="fa-sharp fa-solid fa-rotate-right rotate"></i>
+          </button>
+          <button class="removeBtn" onClick="deleteTask('${list[i].id}')">
+            <i class="fa-solid fa-trash"></i>
+          </button>
+        </div>
+      </div>`;
+        }
+        else {
+            resultHTML += `
+        <div class="task">
+        <div class="taskTitle">
+          <div class="title">${list[i].taskContent}</div>
+        </div>
+        <div class="taskBtns">
+          <button class="checkBtn" onClick="toggleComplete('${list[i].id}')">
+            <i class="fa-solid fa-check"></i>
+          </button>
+          <button class="removeBtn" onClick="deleteTask('${list[i].id}')">
+            <i class="fa-solid fa-trash"></i>
+          </button>
+        </div>
+      </div>`;
+        }
+    }
+    if (taskBoard !== null) {
+        taskBoard.innerHTML = resultHTML;
+    }
+}
+function addTask() {
+    if ((taskInput === null || taskInput === void 0 ? void 0 : taskInput.value) == "" || (taskInput === null || taskInput === void 0 ? void 0 : taskInput.value) == null) {
+        return;
+    }
+    else {
+        let task = {
+            id: randomId(),
+            taskContent: taskInput === null || taskInput === void 0 ? void 0 : taskInput.value,
+            isComplete: false,
+        };
+        taskList.push(task);
+        taskInput.value = "";
+        taskInput.focus();
+        render();
+    }
+}
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,90 @@
+type taskInfo = {
+  id: string;
+  taskContent: string;
+  isComplete: boolean;
+};
 
+const taskInput = document.querySelector<HTMLInputElement>(".taskInput");
+const addBtn = document.querySelector<HTMLButtonElement>(".addBtn");
+const taskBoard = document.querySelector<HTMLDivElement>(".taskBoard");
+let taskList: taskInfo[] = [];
+const tabs = document.querySelectorAll<HTMLDivElement>(".taskTabs div");
+let mode = "all";
+let filterList: taskInfo[] = [];
+const all = document.querySelector<HTMLDivElement>("#all");
+const onGoing = document.querySelector<HTMLDivElement>("#ongoing");
+const done = document.querySelector<HTMLDivElement>("#done");
+
+addBtn?.addEventListener("click", addTask);
+
+taskInput?.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    addTask();
+  }
+});
+
+function randomId() {
+  return "_" + Math.random().toString(36).substring(2, 9);
+}
+
+function render() {
+  let list: taskInfo[] = [];
+  if (mode == "all") {
+    list = taskList;
+  } else if (mode == "ongoing" || mode == "done") {
+    list = filterList;
+  }
+  let resultHTML = "";
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].isComplete == true) {
+      resultHTML += `
+        <div class="task">
+        <div class="taskTitle">
+          <div class="title taskDone" >${list[i].taskContent}</div>
+        </div>
+        <div class="taskBtns">
+          <button class="checkBtn" onClick="toggleComplete('${list[i].id}')">
+          <i class="fa-sharp fa-solid fa-rotate-right rotate"></i>
+          </button>
+          <button class="removeBtn" onClick="deleteTask('${list[i].id}')">
+            <i class="fa-solid fa-trash"></i>
+          </button>
+        </div>
+      </div>`;
+    } else {
+      resultHTML += `
+        <div class="task">
+        <div class="taskTitle">
+          <div class="title">${list[i].taskContent}</div>
+        </div>
+        <div class="taskBtns">
+          <button class="checkBtn" onClick="toggleComplete('${list[i].id}')">
+            <i class="fa-solid fa-check"></i>
+          </button>
+          <button class="removeBtn" onClick="deleteTask('${list[i].id}')">
+            <i class="fa-solid fa-trash"></i>
+          </button>
+        </div>
+      </div>`;
+    }
+  }
+  if (taskBoard !== null) {
+    taskBoard.innerHTML = resultHTML;
+  }
+}
+
+function addTask() {
+  if (taskInput?.value == "" || taskInput?.value == null) {
+    return;
+  } else {
+    let task: taskInfo = {
+      id: randomId(),
+      taskContent: taskInput?.value,
+      isComplete: false,
+    };
+    taskList.push(task);
+    taskInput.value = "";
+    taskInput.focus();
+    render();
+  }
+}
